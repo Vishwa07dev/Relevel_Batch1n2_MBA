8179dee061c137e2c43547c64fc84753f2ff301e
AbhinavChandurkar <abhinavchandurkar55@gmail.com>
2022-05-25 22:48:04 +0530

    fix

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -15,19 +15,19 @@ module.exports = (app) => {
       * mba/api/v1/theatres?zip=<>
       * 
       */
-     app.get("mba/api/v1/theatres" , theatreController.getAllTheatres);
+     app.get("/mba/api/v1/theatres" , theatreController.getAllTheatres);
 
      //Fetching theatre based on id
-     app.get("mba/api/v1/theatres/:id", theatreController.getTheatre);
+     app.get("/mba/api/v1/theatres/:id", theatreController.getTheatre);
 
      //Create theatre
-     app.post("mba/api/v1/theatres", [valReqBody.verifyRequestBody], theatreController.createTheatre);
+     app.post("/mba/api/v1/theatres", [valReqBody.verifyRequestBody], theatreController.createTheatre);
   
 
      //Update theatre 
-     app.put("mba/api/v1/theatres/:id", [valReqBody.verifyRequestBody], theatreController.updateTheatre);
+     app.put("/mba/api/v1/theatres/:id", [valReqBody.verifyRequestBody], theatreController.updateTheatre);
 
      // Delete theatre
-     app.get("mba/api/v1/theatres/:id", theatreController.deleteTheatre);
+     app.get("/mba/api/v1/theatres/:id", theatreController.deleteTheatre);
 
 }
