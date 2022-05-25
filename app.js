842787fc802db4a1e777f57ac3523019a4195c59
AbhinavChandurkar <abhinavchandurkar55@gmail.com>
2022-05-25 21:17:30 +0530

    fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { DB_URL } = require('./configs/dbConfig');
 const { PORT } = require('./configs/serverConfig');
 const Movie = require('./models/movie.model');
-const Theater = require('./models/theatre.model')
+const Theatre = require('./models/theatre.model')
 
 const app = express();
 app.use(express.json());
@@ -13,6 +13,8 @@ app.use(express.json());
 mongoose.connect(DB_URL, async ()=>{
     console.log(`Application is connected to database: ${DB_URL}`);
     
+    await Movie.collection.drop();
+    await Theatre.collection.drop();
     // create movies here
     const movie1 = await Movie.create({
         name: "Puspa",
@@ -66,56 +68,56 @@ mongoose.connect(DB_URL, async ()=>{
     console.log(movie5);
 
     //theater 
-    const theatre1 = await Theater.create({
+    const theatre1 = await Theatre.create({
         name : "PVR",
         description :"nice place to watch movies",
         city : "Pune",
         pinCode : 411028,
-        totalSeats : 100,
-        owner : "xyz"
+        totalSeats : 100
+
     })
     console.log(theatre1);
 
     
-    const theatre2 = await Theater.create({
+    const theatre2 = await Theatre.create({
         name : "INOX",
         description :"corner seat available",
         city : "Pune",
         pinCode : 411033,
-        totalSeats : 100,
-        owner : "xyz"
+        totalSeats : 100
+
     })
     console.log(theatre2);
 
     
-    const theatre3 = await Theater.create({
+    const theatre3 = await Theatre.create({
         name : "Vaibhav Theatre",
         description :"nice place to watch movies",
         city : "Pune",
         pinCode : 411022,
-        totalSeats : 100,
-        owner : "xyz"
+        totalSeats : 100
+
     })
     console.log(theatre3);
 
     
-    const theatre4 = await Theater.create({
+    const theatre4 = await Theatre.create({
         name : "cinepolis",
         description :"nice place to watch movies",
         city : "Pune",
         pinCode : 411133,
-        totalSeats : 100,
-        owner : "xyz"
+        totalSeats : 100
+
     })
     console.log(theatre4);
 
-    const theatre5 = await Theater.create({
+    const theatre5 = await Theatre.create({
         name : "PVR",
         description :"nice place to watch movies",
         city : "Pune",
         pinCode : 411123,
-        totalSeats : 100,
-        owner : "xyz"
+        totalSeats : 100
+
     })
     console.log(theatre5);
 })
