8afb5e99f8e8c0236a60c62b9b1bd7d5fd514bff
vishal <vishalbahure22@gmail.com>
2022-05-25 22:40:54 +0530

    theater

--- app.js
+++ app.js
@@ -12,57 +12,57 @@ app.use(express.json());
 mongoose.connect(DB_URL, async ()=>{
     console.log(`Application is connected to database: ${DB_URL}`);
     
-    // create movies here
-    const movie1 = await Movie.create({
-        name: "Puspa",
-        description:"This is south indian movie.",
-        cast:"Allu Arjun",
-        director:"Puspraj",
-        trailerUrls:"xyz123.com",
-        posterUrls:"xyz123.in",
-        releaseDate: "05-25-2022"
-    })
-    console.log(movie1);
-    const movie2 = await Movie.create({
-        name: "Puspa 2",
-        description:"This is south indian movie.",
-        cast:"Allu Arjun",
-        director:"Puspraj",
-        trailerUrls:"xyz123.com",
-        posterUrls:"xyz123.in",
-        releaseDate: "03-25-2022"
-    })
-    console.log(movie2);
-    const movie3 = await Movie.create({
-        name: "RRR",
-        description:"This is south indian movie.",
-        cast:"Allu Arjun",
-        director:"Puspraj",
-        trailerUrls:"xyz123.com",
-        posterUrls:"xyz123.in",
-        releaseDate: "02-25-2022"
-    })
-    console.log(movie3);
-    const movie4 = await Movie.create({
-        name: "SHAHO",
-        description:"This is south indian movie.",
-        cast:"Allu Arjun",
-        director:"Puspraj",
-        trailerUrls:"xyz123.com",
-        posterUrls:"xyz123.in",
-        releaseDate: "01-25-2022"
-    })
-    console.log(movie4);
-    const movie5 = await Movie.create({
-        name: "BEAST",
-        description:"This is south indian movie.",
-        cast:"Allu Arjun",
-        director:"Puspraj",
-        trailerUrls:"xyz123.com",
-        posterUrls:"xyz123.in",
-        releaseDate: "06-25-2022"
-    })
-    console.log(movie5);
+    // // create movies here
+    // const movie1 = await Movie.create({
+    //     name: "Puspa",
+    //     description:"This is south indian movie.",
+    //     cast:"Allu Arjun",
+    //     director:"Puspraj",
+    //     trailerUrls:"xyz123.com",
+    //     posterUrls:"xyz123.in",
+    //     releaseDate: "05-25-2022"
+    // })
+    // console.log(movie1);
+    // const movie2 = await Movie.create({
+    //     name: "Puspa 2",
+    //     description:"This is south indian movie.",
+    //     cast:"Allu Arjun",
+    //     director:"Puspraj",
+    //     trailerUrls:"xyz123.com",
+    //     posterUrls:"xyz123.in",
+    //     releaseDate: "03-25-2022"
+    // })
+    // console.log(movie2);
+    // const movie3 = await Movie.create({
+    //     name: "RRR",
+    //     description:"This is south indian movie.",
+    //     cast:"Allu Arjun",
+    //     director:"Puspraj",
+    //     trailerUrls:"xyz123.com",
+    //     posterUrls:"xyz123.in",
+    //     releaseDate: "02-25-2022"
+    // })
+    // console.log(movie3);
+    // const movie4 = await Movie.create({
+    //     name: "SHAHO",
+    //     description:"This is south indian movie.",
+    //     cast:"Allu Arjun",
+    //     director:"Puspraj",
+    //     trailerUrls:"xyz123.com",
+    //     posterUrls:"xyz123.in",
+    //     releaseDate: "01-25-2022"
+    // })
+    // console.log(movie4);
+    // const movie5 = await Movie.create({
+    //     name: "BEAST",
+    //     description:"This is south indian movie.",
+    //     cast:"Allu Arjun",
+    //     director:"Puspraj",
+    //     trailerUrls:"xyz123.com",
+    //     posterUrls:"xyz123.in",
+    //     releaseDate: "06-25-2022"
+    // })
+    // console.log(movie5);
 
     const theater1 = await Theater.create({
         name: "PVR",
@@ -117,6 +117,7 @@ mongoose.connect(DB_URL, async ()=>{
 
 
 require("./routes/movie.route")(app);
+require("./routes/theater.route")(app);
 
 
 
