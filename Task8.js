9f478acdd77162fecdfc58b719141becfd38cbd9
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-06-10 20:55:46 +0530

    final task

---
+++ Task8.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * 1. Install and run the Notification service
+ * https://github.com/Vishwa07dev/NotificationService
+ * 
+ * 2.  Make changes in the MBA, so that every successfull payment,
+ * leads to calling the Notification service, which will
+ * asynchronouly send the email to the user
+ * 
+ * 3.Complete the code and do the proper testing.
+ * 
+ */
